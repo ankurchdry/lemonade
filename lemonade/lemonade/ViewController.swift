8f08a67e471aa2dddfb98b4ed37e6d6a17e70299
kostapappas lambros <kostapappas@gmail.com>
2014-12-29 18:44:50 +0200

    first commit

---
+++ lemonade/lemonade/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lemonade
+//
+//  Created by kostapappas lambros on 12/29/14.
+//  Copyright (c) 2014 kostapappas lambros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
